0d524ee5f75f8316c6108d150218dda70401b411
kiwenlau <kiwenlau@126.com>
2015-11-20 20:19:13 +0900

    only mount docker socket for kubelet container which is in charge of starting container

--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -7,7 +7,7 @@ sudo docker run -p 4001 -p 7001 -p 2379 -p  2380 -d --name="etcd" quay.io/coreos
 
 # Run apiserver container
 echo "Starting apiserver container..."
-sudo docker run -p 8080 -p 6443 --link etcd:etcd -d -v /var/run/docker.sock:/var/run/docker.sock --name="apiserver" kiwenlau/kubernetes:1.0.7 kube-apiserver \
+sudo docker run -p 8080 -p 6443 --link etcd:etcd -d --name="apiserver" kiwenlau/kubernetes:1.0.7 kube-apiserver \
                                                                                                     --service-cluster-ip-range=10.0.0.1/24 \
                                                                                                     --insecure-bind-address=0.0.0.0 \
                                                                                                     --etcd_servers=http://etcd:4001 \
@@ -18,13 +18,13 @@ sleep 10
 
 # Run controller-manager container
 echo "Starting controller-manager container..."
-sudo docker run --link apiserver:apiserver -d -v /var/run/docker.sock:/var/run/docker.sock --name="controller-manager" kiwenlau/kubernetes:1.0.7 kube-controller-manager \
+sudo docker run --link apiserver:apiserver -d --name="controller-manager" kiwenlau/kubernetes:1.0.7 kube-controller-manager \
                                                                                                                                      --master=http://apiserver:8080 \
                                                                                                                                      --v=2                                                                                                     
 
 # Run scheduler container
 echo "Starting scheduler container..."
-sudo docker run --link apiserver:apiserver -d -v /var/run/docker.sock:/var/run/docker.sock --name="scheduler" kiwenlau/kubernetes:1.0.7 kube-scheduler \
+sudo docker run --link apiserver:apiserver -d --name="scheduler" kiwenlau/kubernetes:1.0.7 kube-scheduler \
                                                                                                                    --master=http://apiserver:8080 \
                                                                                                                    --v=2 
                                                                                                                   
@@ -50,6 +50,4 @@ echo "Starting kubectl container..."
 sudo docker run --link apiserver:apiserver -e "KUBERNETES_MASTER=http://apiserver:8080" --name="kubectl" --hostname="kubectl" -id kiwenlau/kubernetes:1.0.7 bash 
 
 sudo docker exec -it kubectl bash
-
-#export KUBERNETES_MASTER=http://apiserver:8080
 
