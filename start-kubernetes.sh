e9e4dc959429a549207f53650baf9dfe181407e0
kiwenlau <kiwenlau@126.com>
2015-11-22 16:15:42 +0900

    test

--- start-kubernetes.sh
+++ start-kubernetes.sh
@@ -3,16 +3,16 @@
 # Run ectd container
 echo "Starting ectd container..."
 sudo docker run -d --name="etcd" quay.io/coreos/etcd:v2.2.1 \
-                                                 --addr=127.0.0.1:4001 \
-                                                 --bind-addr=0.0.0.0:4001 \
-                                                 --data-dir=/var/etcd/data 
+                                      --addr=127.0.0.1:4001 \
+                                      --bind-addr=0.0.0.0:4001 \
+                                      --data-dir=/var/etcd/data 
 
 # Run apiserver container
 echo "Starting apiserver container..."
 sudo docker run -d --link etcd:etcd --name="apiserver" kiwenlau/kubernetes:1.0.7 kube-apiserver \
-                                                                                                    --service-cluster-ip-range=10.0.0.1/24 \
-                                                                                                    --insecure-bind-address=0.0.0.0 \
-                                                                                                    --etcd_servers=http://etcd:4001
+                                                         --service-cluster-ip-range=10.0.0.1/24 \
+                                                         --insecure-bind-address=0.0.0.0 \
+                                                         --etcd_servers=http://etcd:4001
 
 sleep 10
 
@@ -27,11 +27,11 @@ sudo docker run -d --link apiserver:apiserver --name="scheduler" kiwenlau/kubern
 # Run kubelet container
 echo "Starting kubelet container..."
 sudo docker run -d --link apiserver:apiserver --pid=host -v /var/run/docker.sock:/var/run/docker.sock --name="kubelet"  kiwenlau/kubernetes:1.0.7 kubelet \
-                                                                                                   --api_servers=http://apiserver:8080 \
-                                                                                                   --address=0.0.0.0 \
-                                                                                                   --hostname_override=127.0.0.1 \
-                                                                                                   --cluster_dns=10.0.0.10 \
-                                                                                                   --cluster_domain="kubernetes.local" 
+                                                                                                                      --api_servers=http://apiserver:8080 \
+                                                                                                                      --address=0.0.0.0 \
+                                                                                                                      --hostname_override=127.0.0.1 \
+                                                                                                                      --cluster_dns=10.0.0.10 \
+                                                                                                                      --cluster_domain="kubernetes.local" 
                                                                                                    
 # Run proxy container
 echo "Starting proxy container..."
@@ -41,5 +41,6 @@ sudo docker run -d --link apiserver:apiserver --privileged --name="proxy" kiwenl
 echo "Starting kubectl container..."                                                                 
 sudo docker run -id --link apiserver:apiserver -e "KUBERNETES_MASTER=http://apiserver:8080" --name="kubectl" --hostname="kubectl" kiwenlau/kubernetes:1.0.7 bash 
 
+#Get into the kubectl container
 sudo docker exec -it kubectl bash
 
